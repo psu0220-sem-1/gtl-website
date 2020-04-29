356f559c6ad522952c2a65c3c1566849c18eed8e
Alexander Ignacz <ignaczalexander@gmail.com>
2020-04-29 18:02:51 +0200

    Add volumes component

---
+++ src/pages/Volumes.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Volumes page
+
+    <b-table :items="volumes"></b-table>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+export default {
+  name: "Volumes",
+  computed: mapState({
+    volumes: (state) => state.volumes.volumes,
+  }),
+  methods: {
+    ...mapActions(["fetchVolumes"]),
+  },
+  created() {
+    this.fetchVolumes();
+  },
+};
+</script>
+
+<style></style>
