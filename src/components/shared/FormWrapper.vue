3de653b2d0d7a0a36d62930d71e6a2bee5194cef
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-01 21:59:21 +0200

    Add wrapper component

---
+++ src/components/shared/FormWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormWrapper",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style>
+</style>
