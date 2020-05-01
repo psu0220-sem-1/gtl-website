33be1c4fbc896dd7f4e03746ca4760111434b6cc
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-01 21:58:45 +0200

    Add constants file

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export default {
+  routes: {
+    ADDRESSES_PAGE: "/addresses",
+    MATERIALS_PAGE: "/materials",
+    VOLUMES_PAGE: "/volumes",
+    CREATE_ADDRESS: "/addresses/create",
+    CREATE_VOLUME: "/volume/create",
+  },
+};
