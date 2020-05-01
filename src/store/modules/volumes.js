8998655a780574e0e21b86cb87ab10acded36dca
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-01 21:58:15 +0200

    Add post, fetch, delete volume action

--- src/store/modules/volumes.js
+++ src/store/modules/volumes.js
@@ -1,12 +1,17 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 import parsers from "../../utils/parsers";
 
 const state = {
   volumes: [],
+  volume: {},
 };
 const mutations = {
   setVolumes(state, volumes) {
     state.volumes = volumes;
+  },
+  setVolume(state, volume) {
+    state.volume = volume;
   },
 };
 const actions = {
@@ -15,6 +20,37 @@ const actions = {
       console.log(res.data);
       const volumes = parsers.parseVolumes(res.data);
       commit("setVolumes", volumes);
+    });
+  },
+  postVolume({ commit }, data) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post("/volume", data)
+        .then((res) => {
+          console.log(res);
+          resolve();
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+  fetchVolume({ commit }, id) {
+    axios.get(`/volume/${id}`).then((res) => {
+      console.log("vol", res.data);
+      commit("setVolume", res.data);
+    });
+  },
+  deleteVolume({ commit, state }, id) {
+    return new Promise((resolve, reject) => {
+      axios
+        .delete(`/volume/${id}`)
+        .then((res) => {
+          console.log(res.data);
+          const volumes = state.volumes.filter((vol) => vol.id !== id);
+          commit("setVolumes", volumes);
+        })
+        .catch((err) => reject(err));
     });
   },
 };
