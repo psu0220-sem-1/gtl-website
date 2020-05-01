1a44fb757ecb569d67ddadb63473b878295a778f
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-01 21:58:34 +0200

    Add address store module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import volumes from "./modules/volumes";
+import address from "./modules/address";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     volumes,
+    address,
   },
 });
